d5c48a2ddb5cb0960bce5f0a05bb52930efcad4d
kawalpreet111 <132731301+kawalpreet111@users.noreply.github.com>
2024-03-13 22:06:49 +0530

    Add files via upload

---
+++ pokemon/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FiHeart } from 'react-icons/fi'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h4>Made by Saksham Jain </h4>
+        <FiHeart className='heart'></FiHeart>
+    </div>
+  )
+}
+
+export default Footer
---
+++ pokemon/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='hnav'>
+        <h1>VAC Project Pokemon App</h1>
+    </div>
+  )
+}
+
+export default Header
